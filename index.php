0b39f634720829189c259d0a896ae72f9bfef9e2
Shatiger <pauamat888@gmail.com>
2024-01-14 18:08:49 +0100

    index.php styles modified

--- index.php
+++ index.php
@@ -9,38 +9,46 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <link rel="stylesheet" href="./css/login.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
     <script src="https://kit.fontawesome.com/b8504978d2.js" crossorigin="anonymous"></script>
 
     <title>Login</title>
 </head>
 <body>
     <main>
+
+        <div class="img-container">
+            <!-- imagen -->
+            <img class="img" src="./img/estudianteisitec.jpg" alt="Estudiando en isitec">
+        </div>
         
-            <div class="img-container">
-                <!-- imagen -->
-                <img class="img" src="./img/estudianteisitec.jpg" alt="Estudiando en isitec">
+        <form action="./index.php" method="POST">
+            <div class="container">
+                <header>
+                    <h1>Welcome to Isitec</h1>
+                    <p>Where learning tech is now isi</p>
+                </header>
+
+                <div>
+                    <label for="username"><i class="fa-regular fa-envelope"></i></label>
+                    <input type="text" placeholder="Mail" name="username" id="username">   
+                </div>
+
+                <div>
+                    <label for="password"><i class="fa-solid fa-key"></i></label>
+                    <input type="text" name="password" placeholder="Password" id="password">
+                </div>
+
+                <div>
+                    <button class="login-btn" type="submit">Login</button>
+                    <button class="signup-btn" type="submit">Sing up</button>
+                </div>
             </div>
             
-            <form action="./index.php" method="POST">
-                <div class="container">
-                    <div>
-                        <label for="username"><i class="fa-regular fa-envelope"></i></label>
-                        <input type="text" placeholder="Mail" name="username" id="username">   
-                    </div>
 
-                    <div>
-                        <label for="password"><i class="fa-solid fa-key"></i></label>
-                        <input type="text" name="password" placeholder="Password" id="password">
-                    </div>
-
-                    <div>
-                        <button type="submit">Login</button>
-                        <button type="submit">Sing up</button>
-                    </div>
-                </div>
-            
-
-            </form>
+        </form>
     </main>
 </body>
 </html>
